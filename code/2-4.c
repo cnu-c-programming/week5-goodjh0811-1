f60b0e5b71d250875546ab3913932b60054e356f
박지현202500924 <goodjh0811@gmail.com>
2026-04-03 09:37:05 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+long my_strtol(char *str, char **endptr) {
+  long res = 0;
+
+  while (                 ){
+
+
+    }
+
+  return res;
+}
+
+int main() {
+  char str[] = "2026hello";
+  char *end;
+
+  long val = my_strtol(str, &end);
+
+  printf("%ld %s\m", val, end);
+  return 0;
+
+}
