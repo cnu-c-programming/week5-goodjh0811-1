4bf3c28d944fd4fa9be89a1aa672c09455eb20d0
박지현202500924 <goodjh0811@gmail.com>
2026-04-03 09:59:15 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int sum_range(int *start, int *end){
-
-
-
-
-
+    int sum 0;
+    while (start < end) {
+      sum += *start;
+      start++;
+    }
+  return sum;
 }
 
 int main() {
