202941b34e89d1c130026042282ce4348dffa155
박지현202500924 <goodjh0811@gmail.com>
2026-04-03 09:12:51 +0900

    Update 1-2.c

---
+++ code/1-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int sum_range(int *start, int *end){
+
+
+
+
+
+}
+
+int main() {
+  int date[] = {10, 20, 30, 40, 40, 60, 70};
+
+  int result = sum_range(&date[1], &date[4]);
+
+  printf("%d\n", result);
+  return 0;
+
+}
