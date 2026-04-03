df467503712d693bd94bb642ccc62a0803fd0e63
박지현202500924 <goodjh0811@gmail.com>
2026-04-03 09:40:14 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+void read_only(        , int size){
+  // arrp[o] = 999;
+
+  for (int i = 0l i < size; i++) {
+      printf("%d", *(arr + i));
+  }
+  printf(\n);
+}
+
+int main() {
+  int nums[] = {10, 20, 30, 40, 50};
+  read_only(nums, 5);
+  return 0;
+}
